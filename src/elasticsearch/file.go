a8086140e8113135fe793eaedfad8564e14c64b7
jelly <haode888@163.com>
2021-08-07 10:56:06 +0800

    init

---
+++ src/elasticsearch/file.go
@@ -0,0 +1,121 @@
+package elasticsearch
+
+import (
+	"bufio"
+	"encoding/json"
+	log "github.com/cihub/seelog"
+	"gopkg.in/cheggaaa/pb.v1"
+	"io"
+	"os"
+	"sync"
+)
+
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+func (m *Migrator) NewFileReadWorker(pb *pb.ProgressBar, wg *sync.WaitGroup) {
+	log.Debug("start reading file")
+	f, err := os.Open(m.Config.DumpInputFile)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	defer f.Close()
+	r := bufio.NewReader(f)
+	lineCount := 0
+	for {
+		line, err := r.ReadString('\n')
+		if io.EOF == err || nil != err {
+			break
+		}
+		lineCount += 1
+		js := map[string]interface{}{}
+
+		err = DecodeJson(line, &js)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		m.DocChan <- js
+		pb.Increment()
+	}
+
+	defer f.Close()
+	log.Debug("end reading file")
+	close(m.DocChan)
+	wg.Done()
+}
+
+func (c *Migrator) NewFileDumpWorker(pb *pb.ProgressBar, wg *sync.WaitGroup) {
+	var f *os.File
+	var err1 error
+
+	if checkFileIsExist(c.Config.DumpOutFile) {
+		f, err1 = os.OpenFile(c.Config.DumpOutFile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		if err1 != nil {
+			log.Error(err1)
+			return
+		}
+
+	} else {
+		f, err1 = os.Create(c.Config.DumpOutFile)
+		if err1 != nil {
+			log.Error(err1)
+			return
+		}
+	}
+
+	w := bufio.NewWriter(f)
+
+READ_DOCS:
+	for {
+		docI, open := <-c.DocChan
+
+		// this check is in case the document is an error with scroll stuff
+		if status, ok := docI["status"]; ok {
+			if status.(int) == 404 {
+				log.Error("error: ", docI["response"])
+				continue
+			}
+		}
+
+		// sanity check
+		for _, key := range []string{"_index", "_type", "_source", "_id"} {
+			if _, ok := docI[key]; !ok {
+				//json,_:=json.Marshal(docI)
+				//log.Errorf("failed parsing document: %v", string(json))
+				break READ_DOCS
+			}
+		}
+
+		jsr, err := json.Marshal(docI)
+		log.Trace(string(jsr))
+		if err != nil {
+			log.Error(err)
+		}
+		n, err := w.WriteString(string(jsr))
+		if err != nil {
+			log.Error(n, err)
+		}
+		w.WriteString("\n")
+		pb.Increment()
+
+		// if channel is closed flush and gtfo
+		if !open {
+			goto WORKER_DONE
+		}
+	}
+
+WORKER_DONE:
+	w.Flush()
+	f.Close()
+
+	wg.Done()
+	log.Debug("file dump finished")
+}
---
+++ src/elasticsearch/scroll.go
@@ -0,0 +1,132 @@
+package elasticsearch
+
+import (
+	"encoding/json"
+	log "github.com/cihub/seelog"
+	"gopkg.in/cheggaaa/pb.v1"
+)
+
+type ScrollAPI interface {
+	GetScrollId() string
+	GetHitsTotal() int
+	GetDocs() []interface{}
+	ProcessScrollResult(c *Migrator, bar *pb.ProgressBar)
+	Next(c *Migrator, bar *pb.ProgressBar) (done bool)
+}
+
+func (scroll *Scroll) GetHitsTotal() int {
+	//fmt.Println("total v0:",scroll.Hits.Total)
+	return scroll.Hits.Total
+}
+
+func (scroll *Scroll) GetScrollId() string {
+	return scroll.ScrollId
+}
+
+func (scroll *Scroll) GetDocs() []interface{} {
+
+	//fmt.Println("docs v0:",scroll.Hits)
+
+	return scroll.Hits.Docs
+}
+
+func (scroll *ScrollV7) GetHitsTotal() int {
+	//fmt.Println("total v7:",scroll.Hits.Total.Value)
+
+	return scroll.Hits.Total.Value
+}
+
+func (scroll *ScrollV7) GetScrollId() string {
+	return scroll.ScrollId
+}
+
+func (scroll *ScrollV7) GetDocs() []interface{} {
+
+	//fmt.Println("docs v7:",scroll.Hits)
+
+	return scroll.Hits.Docs
+}
+
+// Stream from source es instance. "done" is an indicator that the stream is
+// over
+func (s *Scroll) ProcessScrollResult(c *Migrator, bar *pb.ProgressBar) {
+
+	//update progress bar
+	bar.Add(len(s.Hits.Docs))
+
+	// show any failures
+	for _, failure := range s.Shards.Failures {
+		reason, _ := json.Marshal(failure.Reason)
+		log.Errorf(string(reason))
+	}
+
+	// write all the docs into a channel
+	for _, docI := range s.Hits.Docs {
+		//fmt.Println(docI)
+		c.DocChan <- docI.(map[string]interface{})
+	}
+}
+
+func (s *Scroll) Next(c *Migrator, bar *pb.ProgressBar) (done bool) {
+
+	scroll, err := c.SourceESAPI.NextScroll(c.Config.ScrollTime, s.ScrollId)
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+
+	docs := scroll.(ScrollAPI).GetDocs()
+	if docs == nil || len(docs) <= 0 {
+		log.Debug("scroll result is empty")
+		return true
+	}
+
+	scroll.(ScrollAPI).ProcessScrollResult(c, bar)
+
+	//update scrollId
+	s.ScrollId = scroll.(ScrollAPI).GetScrollId()
+
+	return
+}
+
+// Stream from source es instance. "done" is an indicator that the stream is
+// over
+func (s *ScrollV7) ProcessScrollResult(c *Migrator, bar *pb.ProgressBar) {
+
+	//update progress bar
+	bar.Add(len(s.Hits.Docs))
+
+	// show any failures
+	for _, failure := range s.Shards.Failures {
+		reason, _ := json.Marshal(failure.Reason)
+		log.Errorf(string(reason))
+	}
+
+	// write all the docs into a channel
+	for _, docI := range s.Hits.Docs {
+		//fmt.Println(docI)
+		c.DocChan <- docI.(map[string]interface{})
+	}
+}
+
+func (s *ScrollV7) Next(c *Migrator, bar *pb.ProgressBar) (done bool) {
+
+	scroll, err := c.SourceESAPI.NextScroll(c.Config.ScrollTime, s.ScrollId)
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+
+	docs := scroll.(ScrollAPI).GetDocs()
+	if docs == nil || len(docs) <= 0 {
+		log.Debug("scroll result is empty")
+		return true
+	}
+
+	scroll.(ScrollAPI).ProcessScrollResult(c, bar)
+
+	//update scrollId
+	s.ScrollId = scroll.(ScrollAPI).GetScrollId()
+
+	return
+}
